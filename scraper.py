7e8ddb2e829192ff85f4b51d36a682281b4c585a
jjstn-lee <justinlee5483@gmail.com>
2025-02-10 14:55:40 -0500

    able to navigate to finished tournaments screen

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -12,29 +13,24 @@ try:
     WebDriverWait(driver, 20).until(
         EC.title_contains("Valorant")
     )
-except:
-    print("failed to load initial page, quitting...\n")
+except Exception as e:
+    print(f"failed to load initial page: {e}")
     driver.quit()    
 
 try:
-    print("waiting for spoiler pop-up...")
     WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".vfm__content.vfm--outline-none"))
     )
 
-    print("done waiting for spoiler pop up, clicking ok...")
-
+    # find spoiler pop-up button based on position in DOM
     buttons = driver.find_elements(By.CSS_SELECTOR, ".c-button.c-button--full-width")
     spoilers_button = buttons[5]
 
-    # spoilers_button.click()
-
+    # use javascript click
     driver.execute_script("arguments[0].click();", spoilers_button)
 
-    print("clicked ok :)")
-
 except Exception as e:
-    print(f"failed to load spoiler pop-up: {e}")
+    print(f"failed to handle spoiler pop-up: {e}")
     driver.quit()
     exit(0)
 
@@ -42,20 +38,35 @@ except Exception as e:
 # assert "bo3" in driver.title
 # assert "valorant" in driver.title 
 
+
+
+# wait for pop-up first, so matches button should be loaded by now
 try:
-    element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "/valorant/matches/")
-    ))
+    print(f"looking for matches button...")
 
-    matches = driver.find_elements(By.PARTIAL_LINK_TEXT, "/valorant/matches/")
-    print("successfully loaded table, locating matc hes...")
+    tournaments_button = driver.find_element(By.LINK_TEXT, "Tournaments")
+    print(f"clicking tournaments button...")
+    tournaments_button.click()
+    # finished_button = driver.find_element(By.LINK_TEXT, "Finished")
+    # print(f"clicking finished button...")
+    # finished_button.click()
 
-    print(f"len of matches: {len(matches)}")
-    for n in range(len(matches)):
-        print(matches[n].text)
-
-except:
-    print("failed to fetch table, quitting...\n")
+except Exception as e:
+    print(f"failed to click matches or finished button: {e}")
     driver.quit()
 
+try:
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Finished"))
+    )
 
+    finished_button = driver.find_element(By.LINK_TEXT, "Finished")
+    finished_button.click()
+
+except Exception as e:
+    print(f"failed to handle finish button: {e}")
+    driver.quit()
+    exit(0)
+
+
+sleep(10)
